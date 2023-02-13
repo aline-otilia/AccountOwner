77cf5da6ca9c9419742161f665e4f5ce5b9b22ec
Aline Otília <aos_santos@hotmail.com>
2023-02-13 20:24:02 -0300

    Criada a classe estática de serviços e adicionado as chamadas no Program.cs

---
+++ backend/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccountOwnerServer.Extensions;
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services){
+            services.AddCors(options => {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin() // WithOrigin("Dominio.com.br")
+                    .AllowAnyMethod() // WithMethods("POST","GET")
+                    .AllowAnyHeader()); //
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services){
+            services.Configure<IISOptions>(options =>{});
+        }
+    }
