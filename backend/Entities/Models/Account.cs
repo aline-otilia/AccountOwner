58dbea264f325904a03120b4821fab0361a182d3
Aline Otília <aos_santos@hotmail.com>
2023-03-03 22:27:34 -0300

    Criação do projeto de classe e conexão com o banco

---
+++ backend/Entities/Models/Account.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models;
+
+public class Account { 
+
+    [Key]
+    public Guid AccountId { get; set; }
+
+    [Required]
+    public DateTime DateCreated { get; set; }
+
+    [ForeignKey(nameof(Owner))]
+    public Guid OwnerId { get; set; }
+    public Owner? Owner { get; set; }
+}
