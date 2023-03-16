2292f2e194d19761b563647ef2de4c2b795efe20
Adrian José Pereira Santos <adrianjpsantoos@gmail.com>
2023-03-16 20:29:35 -0300

    Criado Ações no OwnerController

--- backend/AccountOwnerServer/Controllers/OwnerController.cs
+++ backend/AccountOwnerServer/Controllers/OwnerController.cs
@@ -2,35 +2,94 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Contracts;
+using Entities.DataTransferObjects;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountOwnerServer.Controllers;
 
 [Route("api/owner")]
 [ApiController]
-[Route("api/[controller]")]
 public class OwnerController : ControllerBase
 {
     private ILoggerManager _logger;
     private IRepositoryWrapper _repository;
+    private IMapper _mapper;
 
-    public OwnerController(ILoggerManager logger,IRepositoryWrapper repository)
+    public OwnerController(ILoggerManager logger, IRepositoryWrapper repository, IMapper mapper)
     {
         _logger = logger;
         _repository = repository;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    public IActionResult GetAllOwners(){
-        try{
+    public IActionResult GetAllOwners()
+    {
+        try
+        {
             var owners = _repository.Owner.GetAllOwners();
             _logger.LogInfo($"Retornando todos os owners do banco de dados.");
-            return Ok(owners);
+
+            var ownersResult = _mapper.Map<IEnumerable<OwnerDto>>(owners);
+            return Ok(ownersResult);
         }
-        catch(Exception ex){
+        catch (Exception ex)
+        {
             _logger.LogError($"Ocorreu um erro no método GetAllOwners: {ex.Message}");
-            return StatusCode(500,"Err Interno do Servidor");
+            return StatusCode(500, "Erro Interno do Servidor");
+        }
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetOwnerById(Guid id)
+    {
+        try
+        {
+            var owner = _repository.Owner.GetOwnerById(id);
+            if (owner is null)
+            {
+                _logger.LogError($"Owner com Id: {id},não encontrado.");
+                return NotFound();
+            }
+            else
+            {
+                _logger.LogInfo($"Retornando o owner com Id: {id} ");
+
+                var ownerResult = _mapper.Map<OwnerDto>(owner);
+                return Ok(ownerResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Ocorreu um erro no Método GetOwnerById: {ex.Message}");
+            return StatusCode(500, "Erro Interno do Servidor");
+        }
+    }
+
+    [HttpGet("{id}/account")]
+    public IActionResult GetOwnerWithDetails(Guid id)
+    {
+        try
+        {
+            var owner = _repository.Owner.GetOwnerWithDetails(id);
+            if (owner == null)
+            {
+                _logger.LogError($"Owner com Id: {id}, não encontrado.");
+                return NotFound();
+            }
+            else
+            {
+                _logger.LogInfo($"Retornando o owner com detalhes e Id: {id}");
+                var ownerResult = _mapper.Map<OwnerDto>(owner);
+                return Ok(ownerResult);
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Ocorreu um erro no método GetOwnerWithDetails: {ex.Message}");
+            return StatusCode(500, "Erro Interno do Servidor");
         }
     }
 }
--- backend/Contracts/OwnerRepository.cs
+++ backend/Contracts/OwnerRepository.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Entities;
 using Entities.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Contracts;
 
@@ -17,5 +18,14 @@ public class OwnerRepository : RepositoryBase<Owner>, IOwnerRepository
     public IEnumerable<Owner> GetAllOwners()
     {
         return FindAll().OrderBy(ow => ow.Name).ToList();
+    }
+
+    public Owner GetOwnerById(Guid ownerId)
+    {
+        return FindByCondition(owner => owner.Id.Equals(ownerId)).FirstOrDefault();
+    }
+
+    public Owner GetOwnerWithDetails(Guid ownerId){
+        return FindByCondition(owner => owner.Id.Equals(ownerId)).Include(ac => ac.Accounts).FirstOrDefault();
     }
 }
